bd3b1aeaba90f550b0fd6b8174e21d3b1f965ea9
andreszpyt <andre.szpyt@gmail.com>
2026-04-01 11:34:09 -0300

    feat(scheduler): implement dynamic trigger calculation based on clinic lead time

--- src/main/java/com/dto/WhatsAppWebhook.java
+++ src/main/java/com/dto/WhatsAppWebhook.java
@@ -1,16 +1,21 @@
 package com.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record WhatsAppWebhook(String instance, Data data) {
 
     @JsonIgnoreProperties(ignoreUnknown = true)
-    public record Data(String remoteJid, String pushName, Message message) {
+    public record Data(
+            @JsonProperty("remoteJid") String remoteJid,
+            String pushName,
+            Message message,
+            String messageType) {
 
         @JsonIgnoreProperties(ignoreUnknown = true)
-        public record Message(String conversation) {}
-
+        public record Message(
+                String conversation,
+                String base64) {}
     }
-
 }
